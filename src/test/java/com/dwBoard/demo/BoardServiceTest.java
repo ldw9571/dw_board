813ac6a536bfbded4461d32317fec306600434ef
이동우 <myblog0419@naver.com>
2024-11-29 00:11:32 +0900

    ddl-auto : update 변경

---
+++ src/test/java/com/dwBoard/demo/BoardServiceTest.java
@@ -0,0 +1,37 @@
+package com.dwBoard.demo;
+
+import com.dwBoard.demo.Entity.BoardEntity;
+import com.dwBoard.demo.dto.BoardRequestDTO;
+import com.dwBoard.demo.repository.BoardRepository;
+import com.dwBoard.demo.service.BoardService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+public class BoardServiceTest {
+
+    @Autowired
+    BoardService boardService;
+    @Autowired
+    BoardRepository boardRepository;
+
+    @Test
+    void save(){
+        //given
+        BoardRequestDTO dto = new BoardRequestDTO();
+        dto.setTitle("제목");
+        dto.setContent("내용");
+        dto.setWriter("작성자");
+
+        //when
+        BoardEntity write = boardService.write(dto);
+
+        //then
+        Long id = write.getId();
+        System.out.println("id = " + id);
+
+    }
+}
