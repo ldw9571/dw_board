f79a2c15636e177240cf6e2e4db771889882fdf9
myblog <myblog0419@naver.com>
2024-12-08 18:38:03 +0900

    api 버전 시작 -restController -spring validated

---
+++ src/main/java/com/dwBoard/demo/dto/SuccessResponse.java
@@ -0,0 +1,16 @@
+package com.dwBoard.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SuccessResponse {
+    private String code;
+    private String message;
+    private String redirectUrl;
+}
