c48bbb60cd18b651587131311bafc6b0e8ebf880
이동우 <myblog0419@naver.com>
2024-12-11 23:54:15 +0900

    DI 추가

--- src/main/java/com/dwBoard/demo/dto/BoardRequestDTO.java
+++ src/main/java/com/dwBoard/demo/dto/BoardRequestDTO.java
@@ -9,7 +9,7 @@ import lombok.Setter;
 @Getter
 public class BoardRequestDTO {
 
-    private Long index;
+    private Long id;
 
     @NotNull(message = "제목 입력 후 재등록해주세요.")
     @Size(min = 1, max = 100, message = "제목은 1자 이상 100자 이하로 작성해야 합니다.")
